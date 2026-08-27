30a666a73f090194bae4acfb1c42256dab6f6692
Jose Alfredo <ja125garcia@gmail.com>
2026-08-27 13:37:36 -0600

    Load .env in dev and allow BASE_URL host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,11 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Add configured BASE_URL host to allowed hosts in development.
+  if ENV["BASE_URL"].present?
+    config.hosts << URI(ENV["BASE_URL"]).host
+  end
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
